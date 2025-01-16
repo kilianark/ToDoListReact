5da6035234de5693b51387185b2177bcd9792bdb
Kilian <kilian08momo@gmail.com>
2025-01-16 12:58:16 +0100

    added key to id in task model

--- BackEnd/Models/Task.cs
+++ BackEnd/Models/Task.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BackEnd.Models
 {
     public class Task
     {
+        [Key]
         public required int id {get; set;}
         public required string title {get; set;}
         public bool completed {get; set;}
